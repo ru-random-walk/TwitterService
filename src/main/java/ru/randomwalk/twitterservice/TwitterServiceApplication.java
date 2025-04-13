7bc67aba68ee97107d4226a3f5ae9eb9767e91d0
Nikita Kuritsyn <martyz89@yandex.ru>
2025-04-13 10:36:58 +0300

    Add path for swagger

--- src/main/java/ru/randomwalk/twitterservice/TwitterServiceApplication.java
+++ src/main/java/ru/randomwalk/twitterservice/TwitterServiceApplication.java
@@ -1,9 +1,16 @@
 package ru.randomwalk.twitterservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/twitter", description = "Twitter service url")
+        }
+)
 public class TwitterServiceApplication {
 
     public static void main(String[] args) {
