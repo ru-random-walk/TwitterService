d479c1dce9fd8aae2b4f0c0b529a9ddcbedb4819
Nikita Kuritsyn <martyz89@yandex.ru>
2025-04-16 12:58:15 +0300

    Use virtual threads for notification sending

--- src/main/java/ru/randomwalk/twitterservice/service/NotificationSendingService.java
+++ src/main/java/ru/randomwalk/twitterservice/service/NotificationSendingService.java
@@ -1,8 +1,7 @@
 package ru.randomwalk.twitterservice.service;
 
-import com.google.firebase.messaging.FirebaseMessagingException;
 import ru.random.walk.dto.SendNotificationEvent;
 
 public interface NotificationSendingService {
-    void sendNotification(SendNotificationEvent event) throws FirebaseMessagingException;
+    void sendNotification(SendNotificationEvent event);
 }
